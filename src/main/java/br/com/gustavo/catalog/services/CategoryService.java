3e1e076cb1693ba999ba7be598277788996a365f
Gustavo Caçador <gustavoirl456@outlook.com>
2025-05-12 14:11:04 -0300

    feat: buscando categoria por id

--- src/main/java/br/com/gustavo/catalog/services/CategoryService.java
+++ src/main/java/br/com/gustavo/catalog/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +23,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category category = obj.get();
+        return new CategoryDTO(category);
     }
 }
