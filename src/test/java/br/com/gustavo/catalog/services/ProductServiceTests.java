e8569e25d8b860f983dad4160e49f36d98e7254e
Gustavo Caçador <gustavoirl456@outlook.com>
2025-05-19 14:44:52 -0300

    Teste delete lança DatabaseException quando id dependente

--- src/test/java/br/com/gustavo/catalog/services/ProductServiceTests.java
+++ src/test/java/br/com/gustavo/catalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package br.com.gustavo.catalog.services;
 
 import br.com.gustavo.catalog.repositories.ProductRepository;
+import br.com.gustavo.catalog.services.exceptions.DatabaseException;
 import br.com.gustavo.catalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -23,20 +25,31 @@ public class ProductServiceTests {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
-        nonExistingId = 34L;
+        nonExistingId = 2L;
+        dependentId = 3L;
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
-
-        Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(nonExistingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
 
         // se existe id eu retorno true
         Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
         // se n existe id eu retorno false
         Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
+
+        Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            productService.delete(dependentId);
+        });
     }
 
     @Test
@@ -59,5 +72,4 @@ public class ProductServiceTests {
         // verifica se o metodo deleteById foi chamado nessa ação do teste acima
         Mockito.verify(productRepository, Mockito.times(1)).deleteById(existingId);
     }
-
 }
