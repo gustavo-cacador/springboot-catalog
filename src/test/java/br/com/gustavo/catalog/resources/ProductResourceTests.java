51ef06584d656528f65cd68120be08fab4366a0a
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-05-20 21:52:11 -0300

    Testando o update

--- src/test/java/br/com/gustavo/catalog/resources/ProductResourceTests.java
+++ src/test/java/br/com/gustavo/catalog/resources/ProductResourceTests.java
@@ -4,6 +4,7 @@ import br.com.gustavo.catalog.dto.ProductDTO;
 import br.com.gustavo.catalog.services.ProductService;
 import br.com.gustavo.catalog.services.exceptions.ResourceNotFoundException;
 import br.com.gustavo.catalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +33,9 @@ public class ProductResourceTests {
 
     @MockitoBean
     private ProductService productService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private Long existingId;
     private Long nonExistingId;
@@ -47,11 +53,17 @@ public class ProductResourceTests {
 
         when(productService.findAllPaged(any())).thenReturn(page);
 
-        // simulando o comportamento do id existente
+        // simulando o comportamento para buscar com id com id existente
         when(productService.findById(existingId)).thenReturn(productDTO);
 
-        // simulando o comportamento do id não existente
+        // simulando o comportamento para buscar por id com id não existente
         when(productService.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+        // simulando o comportamento para atualizar com id existente
+        when(productService.update(eq(existingId), any())).thenReturn(productDTO);
+
+        // simulando o comportamento para atualizar com id não existente
+        when(productService.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
     }
 
     // findAll do meu controller deveria retornar uma pagina de Produtos
@@ -86,11 +98,47 @@ public class ProductResourceTests {
         result.andExpect(jsonPath("$.description").exists());
     }
 
+    // findById deveria retornar uma exceção qnd passamos um id não existente
     @Test
     public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
 
         ResultActions result =
                 mockMvc.perform(get("/products/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+
+    // update deveria retornar um produtoDTO quando passamos um id existente
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+        // convertendo um objeto java (produtoDTO) em string (formato json)
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").exists());
+        result.andExpect(jsonPath("$.description").exists());
+    }
+
+    // update deveria retornar uma exceção quando passamos um id não existente
+    @Test
+    public void updateShouldReturnONotFoundWhenIdDoesNotExists() throws Exception {
+
+        // convertendo um objeto java (produtoDTO) em string (formato json)
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", nonExistingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON));
 
         result.andExpect(status().isNotFound());
