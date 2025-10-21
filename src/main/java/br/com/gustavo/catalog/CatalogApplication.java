8154bb7960a16d2cba71c65e35b806442cf01886
Gustavo <gustavoirl456@outlook.com>
2025-10-20 22:44:31 -0300

    implementando variaveis de ambiente e fazendo o primeiro teste de upload de imagem com s3

--- src/main/java/br/com/gustavo/catalog/CatalogApplication.java
+++ src/main/java/br/com/gustavo/catalog/CatalogApplication.java
@@ -1,13 +1,23 @@
 package br.com.gustavo.catalog;
 
+import br.com.gustavo.catalog.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogApplication {
+public class CatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		s3service.uploadFile("C:\\Users\\Gustavo\\Desktop\\download.png");
+	}
 }
